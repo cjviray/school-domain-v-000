547bdcd8e2c8d37777fad1b9e93fb18ad0c49124
Christian V <viray.christianj@gmail.com>
2019-01-09 06:35:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class school
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if roster[grade] = name
+    else
+      roster[grade]
+    end 
+    roster 
+  end 
+  
+  
+ end 
