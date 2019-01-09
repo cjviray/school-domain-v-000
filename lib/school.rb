15d8979e6d2036d6beffeb4e89f516a43d995335
Christian V <viray.christianj@gmail.com>
2019-01-09 07:10:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,35 +1,27 @@
-class School
-
-  attr_accessor :name, :roster
+class school
 
   def initialize(name)
     @name = name
     @roster = {}
   end
-
+  
   def add_student(name, grade)
-    #if roster already has grade 9,
-    if roster[grade] #check if it already exists
-      roster[grade] << name #can shovel in if it already exists and points to array
+    if roster[grade]
+      roster[grade] << name
     else
-    #Otherwise create the key of grade 9, and point to an empty array
       roster[grade] = []
       roster[grade] << name
-    end
-  	 roster #return roster at the end
-  end
-
+    end 
+      roster 
+  end 
+  
   def grade(grade)
-    @roster[grade]
-  end
-
+    @roster[grade] 
+  end 
+  
   def sort
     @roster.each do |grade, name|
       name.sort! #{|a,b| a <=> b}
     end
-  end
-
-
-
-
-end
+  end 
+ end 
